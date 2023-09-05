07fb359f3e3753551dcb8ee47a3a66b385796255
siranovel <siranovel@gmail.com>
2023-09-05 10:36:29 +0900

    Add files via upload

---
+++ ext/hellojava/Hoge.java
@@ -0,0 +1,9 @@
+public class Hoge {
+    public String getFoo() {
+        return "Foo";
+    }
+    public Calc getCalc(int value1, int value2) {
+        return new Calc(value1, value2);
+    }
+}
+
