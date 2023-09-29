283deb250a28269fe52d1822e351f3ac41ea1744
siranovel <siranovel@gmail.com>
2023-09-29 14:01:40 +0900

    Add files via upload

--- sample/samplez.rb
+++ sample/samplez.rb
@@ -1,18 +1,14 @@
 require 'hellojava'
 
-class HelloJavaLibTest
-    def func_aTest()
-        HelloJavaLib::func_a()
+RSpec.describe HelloJavaLib do
+    it 'func_a test' do
+        expect(HelloJavaLib::func_a()).to eq nil
     end
-    def func_bTest()
-        HelloJavaLib::func_b()
+    it 'func_b test' do
+        expect(HelloJavaLib::func_b()).to eq nil
     end
-    def func_cTest()
-        HelloJavaLib::func_c(1, 2)
+    it 'func_c test' do
+        expect(HelloJavaLib::func_c(2, 3)).to eq nil
     end
 end
 
-tst = HelloJavaLibTest.new
-tst.func_aTest()
-tst.func_bTest()
-tst.func_cTest()
