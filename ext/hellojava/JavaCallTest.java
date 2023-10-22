055c0f6bf578e7f2d04ca4bb67a61af4b465fec6
siranovel <siranovel@gmail.com>
2023-10-22 11:24:18 +0900

    Add files via upload

--- ext/hellojava/JavaCallTest.java
+++ ext/hellojava/JavaCallTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class JavaCallTest {
     public static String str1() {
         return "str1";
@@ -5,5 +7,15 @@ public class JavaCallTest {
     public String str2() {
         return "str2";
     }
+    public static void sub1(String fname, double[][] vals) {
+        System.out.printf("fname:%s size:%d\n", fname, vals.length);
+        for (int i = 0; i < vals.length; i++) {
+            System.out.printf("vals[%d] size:%d\n", i, vals[i].length);
+            for (int j = 0; j < vals[i].length; j++) {
+                System.out.printf(" %f", vals[i][j]);
+            }
+            System.out.println();
+        }
+    } 
 }
 
